8bd0eb0fe3ea37af1256f8b2fb76796e4f705705
Holewalker <dani75esp@gmail.com>
2023-03-23 11:51:59 +0100

    initial commit

---
+++ app/src/main/java/com/svalero/ermandroidapp/utils/SessionUtil.java
@@ -0,0 +1,42 @@
+package com.svalero.ermandroidapp.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SessionUtil {
+    private SharedPreferences prefs;
+
+    public SessionUtil(Context cntx) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(cntx);
+    }
+
+    public void setUserId(long userId) {
+        prefs.edit().putLong("userId", userId).commit();
+    }
+
+    public long getUserId() {
+        return prefs.getLong("userId", 0L);
+    }
+
+    public void setUserName(String userName) {
+        prefs.edit().putString("userName", userName).commit();
+    }
+
+    public String getUserName() {
+        return prefs.getString("userName", "");
+    }
+}
+
+
+
+/*
+USO:
+        private SessionUtil session;//global variable
+        session = new SessionUtil(Login.this); //damos contexto en onCreate
+        session set cuando hagamos login.
+
+        Despues recuperamos con:
+        session.getUserId()
+        session.getUsername()
+ */
